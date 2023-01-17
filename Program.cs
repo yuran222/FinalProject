a22fd79fca9ca47688d6e400cecb2c073a734cb5
yuran222 <yuran123456@gmail.com>
2023-01-18 01:53:22 +0300

    создал методы для заполнения массива и вывода его в консоль

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] CreateArray(int lenght)
+{
+    string[] array = new string[lenght];
+    Console.WriteLine($"Введите {lenght} текстовых или символьных сообщения");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"\"{array[i]}\"" + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
